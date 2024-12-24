b86e4c0e1a778c93ca77d92290e0711226fb3e4e
Wsx <211180050@smail.nju.edu.cn>
2024-12-24 15:01:25 +0800

    update1224

--- segmentation-jdm/mmseg/models/segmentors/__init__.py
+++ segmentation-jdm/mmseg/models/segmentors/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import BaseSegmentor
-from .cascade_encoder_decoder import CascadeEncoderDecoder
+# from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
 
-__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder']
+__all__ = ['BaseSegmentor', 'EncoderDecoder']
